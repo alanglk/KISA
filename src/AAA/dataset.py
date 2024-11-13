58c43f0adf05ed518372e6a4344a568ffe811827
alang_lk <ag6154lk@gmail.com>
2024-11-13 17:16:03 +0100

    URL de Scikit LOF añadida

--- src/AAA/dataset.py
+++ src/AAA/dataset.py
@@ -5,15 +5,17 @@
 # Wine Dataset:
 # https://archive.ics.uci.edu/dataset/109/wine
 #
+# Scikit LOF:
+# https://scikit-learn.org/dev/auto_examples/neighbors/plot_lof_outlier_detection.html
+#
 # Idea: analizar el dataset en busca de outliers (probar con todas las clases)
 # LOF es independiente del número de clases
 # Eliminar la columna de la clase sobre el conjunto de datos para LOF
 # Obtener la gráfica de outlierness (eje X) vs numero de instancias (eje Y)
 #
-
 # Entrenar un autoencoder con datos de vino tipo 2 (generar train, val y test)
 # Medir el rendimiento del autoencoder frente al test de los vinos tipo 2
 # Finalmente, medir el error de recontrucción cuando hay inputs de vinos tipo 1 y 3 
-
+#
 
 
