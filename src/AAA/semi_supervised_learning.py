75d9164e30348bf9a6466dad1d7e5665cff4606e
alang_lk <ag6154lk@gmail.com>
2024-11-18 18:31:43 +0100

    One class classification empezado

---
+++ src/AAA/semi_supervised_learning.py
@@ -0,0 +1,18 @@
+# Aprendizaje Automático Avanzado (AAA)
+# Semi-Supervised Learning
+# Alan García Justel
+# 
+#
+# Wine Dataset:
+# https://archive.ics.uci.edu/dataset/109/wine
+#
+# Idea: Empezar buscando un algoritmo de self-training en python
+# y utilizar el Wine dataset para jugar a quitar etiquetas para obtener
+# la gráfica del código de R: semi_supervised_learning.R
+# Finalmente, probar una implementación del algoritmo EM en python. Puede
+# estar interesante probar a generar datos sintéticos semietiquetados de 
+# dos gausianas y entrenar el EM, para luego comparar qué pasa si le 
+# damos datos de dos distribuciones exponenciales al EM. 
+# 
+
+
